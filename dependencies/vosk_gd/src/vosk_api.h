03dab9e15b589d2c3f9df89129e7ab6d9be95d21
Eirik <tapawingo.actual@pm.me>
2025-08-27 16:13:18 +0200

    fix: mangled header

--- dependencies/vosk_gd/src/vosk_api.h
+++ dependencies/vosk_gd/src/vosk_api.h
@@ -1,11 +1,18 @@
-/**
- * @file vosk_api.h
- * @author Eirik
- * @brief This header contains the C API for Vosk speech recognition system
- * @version 0.1
- * @date 2025-08-27
- * 
- */
+// Copyright 2020-2021 Alpha Cephei Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/* This header contains the C API for Vosk speech recognition system */
 
 #ifndef VOSK_API_H
 #define VOSK_API_H
@@ -16,14 +23,12 @@ extern "C" {
 
 /** Model stores all the data required for recognition
  *  it contains static data and can be shared across processing
- *  threads. 
- */
+ *  threads. */
 typedef struct VoskModel VoskModel;
 
 
 /** Speaker model is the same as model but contains the data
- *  for speaker identification. 
- */
+ *  for speaker identification. */
 typedef struct VoskSpkModel VoskSpkModel;
 
 
@@ -31,8 +36,7 @@ typedef struct VoskSpkModel VoskSpkModel;
  *  Each recognizer usually runs in own thread and takes audio as input.
  *  Once audio is processed recognizer returns JSON object as a string
  *  which represent decoded information - words, confidences, times, n-best lists,
- *  speaker information and so on 
- */
+ *  speaker information and so on */
 typedef struct VoskRecognizer VoskRecognizer;
 
 
@@ -50,8 +54,7 @@ typedef struct VoskBatchRecognizer VoskBatchRecognizer;
 /** Loads model data from the file and returns the model object
  *
  * @param model_path: the path of the model on the filesystem
- * @returns model object or NULL if problem occured 
- */
+ * @returns model object or NULL if problem occured */
 VoskModel *vosk_model_new(const char *model_path);
 
 
@@ -59,8 +62,7 @@ VoskModel *vosk_model_new(const char *model_path);
  *
  *  The model object is reference-counted so if some recognizer
  *  depends on this model, model might still stay alive. When
- *  last recognizer is released, model will be released too. 
- */
+ *  last recognizer is released, model will be released too. */
 void vosk_model_free(VoskModel *model);
 
 
@@ -68,16 +70,14 @@ void vosk_model_free(VoskModel *model);
  * @param word: the word
  * @returns the word symbol if @param word exists inside the model
  * or -1 otherwise.
- * Reminding that word symbol 0 is for <epsilon> 
- */
+ * Reminding that word symbol 0 is for <epsilon> */
 int vosk_model_find_word(VoskModel *model, const char *word);
 
 
 /** Loads speaker model data from the file and returns the model object
  *
  * @param model_path: the path of the model on the filesystem
- * @returns model object or NULL if problem occured 
- */
+ * @returns model object or NULL if problem occured */
 VoskSpkModel *vosk_spk_model_new(const char *model_path);
 
 
@@ -85,8 +85,7 @@ VoskSpkModel *vosk_spk_model_new(const char *model_path);
  *
  *  The model object is reference-counted so if some recognizer
  *  depends on this model, model might still stay alive. When
- *  last recognizer is released, model will be released too. 
- */
+ *  last recognizer is released, model will be released too. */
 void vosk_spk_model_free(VoskSpkModel *model);
 
 /** Creates the recognizer object
@@ -97,8 +96,7 @@ void vosk_spk_model_free(VoskSpkModel *model);
  *  @param sample_rate The sample rate of the audio you going to feed into the recognizer.
  *                     Make sure this rate matches the audio content, it is a common
  *                     issue causing accuracy problems.
- *  @returns recognizer object or NULL if problem occured 
- */
+ *  @returns recognizer object or NULL if problem occured */
 VoskRecognizer *vosk_recognizer_new(VoskModel *model, float sample_rate);
 
 
@@ -113,8 +111,7 @@ VoskRecognizer *vosk_recognizer_new(VoskModel *model, float sample_rate);
  *                     Make sure this rate matches the audio content, it is a common
  *                     issue causing accuracy problems.
  *  @param spk_model speaker model for speaker identification
- *  @returns recognizer object or NULL if problem occured 
- */
+ *  @returns recognizer object or NULL if problem occured */
 VoskRecognizer *vosk_recognizer_new_spk(VoskModel *model, float sample_rate, VoskSpkModel *spk_model);
 
 
@@ -136,8 +133,7 @@ VoskRecognizer *vosk_recognizer_new_spk(VoskModel *model, float sample_rate, Vos
  *  @param grammar The string with the list of phrases to recognize as JSON array of strings,
  *                 for example "["one two three four five", "[unk]"]".
  *
- *  @returns recognizer object or NULL if problem occured 
- */
+ *  @returns recognizer object or NULL if problem occured */
 VoskRecognizer *vosk_recognizer_new_grm(VoskModel *model, float sample_rate, const char *grammar);
 
 
@@ -146,8 +142,7 @@ VoskRecognizer *vosk_recognizer_new_grm(VoskModel *model, float sample_rate, con
  * Can add speaker recognition model to already created recognizer. Helps to initialize
  * speaker recognition for grammar-based recognizer.
  *
- * @param spk_model Speaker recognition model 
- */
+ * @param spk_model Speaker recognition model */
 void vosk_recognizer_set_spk_model(VoskRecognizer *recognizer, VoskSpkModel *spk_model);
 
 
