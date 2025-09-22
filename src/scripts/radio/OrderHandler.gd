0304ea6213cf613194be86e235109af0d612b09d
tobias <tobiadsa@stud.ntnu.no>
2025-09-22 17:45:24 +0200

    finishing issue 8

--- src/scripts/radio/OrderHandler.gd
+++ src/scripts/radio/OrderHandler.gd
@@ -1,14 +1,13 @@
 extends Node
-## catches when an invalid command is emmited from parsed order and plays ana audio to inform the player
+## Catches when an invalid command is emitted from parsed order and plays an audio to inform the player
 
-# Called when the node enters the scene tree for the first time.
+var error_player: AudioStreamPlayer2D
+
 func _ready() -> void:
 	OrdersParser.parse_error.connect(_on_parseError)
-
+	error_player = get_node("/root/HQTable/Radio/errorSound")
 
 func _on_parseError(error: String) -> void:
-	print("parsing failed and cought")
-	var error_sound = get_node("errorSound") as AudioStreamPlayer
-	error_sound.play()
-	
-	
+	print("error: %s, playing audio..." % error)
+	if error_player:
+		error_player.play()
