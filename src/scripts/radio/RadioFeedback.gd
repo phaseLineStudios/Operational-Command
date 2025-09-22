2b65d714fcf9f68dced38f2405c939774c2c0c68
Eirik <tapawingo.actual@pm.me>
2025-09-22 19:35:02 +0200

    refactor: move and rename orderhandler to RadioFeedback
    Rename to RadioFeedback
    Unload as singleton and instead load as a subnode of `radio.tscn`

--- src/scripts/radio/RadioFeedback.gd
+++ src/scripts/radio/RadioFeedback.gd
@@ -1,11 +1,10 @@
 extends Node
 ## Catches when an invalid command is emitted from parsed order and plays an audio to inform the player
 
-var error_player: AudioStreamPlayer2D
+@onready var error_player: AudioStreamPlayer2D = %ErrorSound
 
 func _ready() -> void:
 	OrdersParser.parse_error.connect(_on_parseError)
-	error_player = get_node("/root/HQTable/Radio/errorSound")
 
 func _on_parseError(error: String) -> void:
 	print("error: %s, playing audio..." % error)
