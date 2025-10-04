350c2bf1f9c51bab3ac7df92d003d794096df139
Gian <gian.rathgeb@bluewin.ch>
2025-10-04 18:28:06 +0200

    feat: added the radiofeedback for ammo

--- src/scripts/radio/RadioFeedback.gd
+++ src/scripts/radio/RadioFeedback.gd
@@ -6,6 +6,25 @@ class_name RadioFeedback
 
 func _ready() -> void:
 	OrdersParser.parse_error.connect(_on_parseError)
+	
+	var ammo := get_tree().get_first_node_in_group("AmmoSystem") as AmmoSystem
+	if ammo:
+		bind_ammo(ammo)
+
+func bind_ammo(ammo: AmmoSystem) -> void:
+	ammo.ammo_low.connect(_on_ammo_low)
+	ammo.ammo_critical.connect(_on_ammo_critical)
+	ammo.ammo_empty.connect(_on_ammo_empty)
+	ammo.resupply_started.connect(_on_resupply_started)
+	ammo.resupply_completed.connect(_on_resupply_completed)
+
+# Not sure how this should be implemented, so I stay with this
+func _on_ammo_low(uid: String) -> void:       LogService.info("%s: Bingo ammo" % uid, "Radio")
+func _on_ammo_critical(uid: String) -> void:  LogService.info("%s: Ammo critical" % uid, "Radio")
+func _on_ammo_empty(uid: String) -> void:     LogService.info("%s: Winchester" % uid, "Radio")
+func _on_resupply_started(src, dst):          LogService.info("%s -> %s: Resupplying" % [src, dst], "Radio")
+func _on_resupply_completed(src, dst):        LogService.info("%s -> %s: Resupply complete" % [src, dst], "Radio")
+
 
 func _on_parseError(error: String) -> void:
 	LogService.error("error: %s, playing audio..." % error, "RadioFeedback.gd:11")
