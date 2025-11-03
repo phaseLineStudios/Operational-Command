c5ff8c68ed1c0f25c1d8b31b953020abd4608af3
Eirik <tapawingo.actual@pm.me>
2025-11-03 15:54:41 +0100

    fix: formatting and syntax

--- src/scripts/editors/WeatherDialog.gd
+++ src/scripts/editors/WeatherDialog.gd
@@ -49,9 +49,9 @@ var _second := 0
 
 func _ready():
 	_regex.compile("^[0-9]*$")
-	
+
 	var real_date := Time.get_date_dict_from_system()
-	
+
 	for year in range(YEAR_START, real_date.year):
 		date_year.add_item(str(year), year)
 	for month in Month.keys():
@@ -59,22 +59,22 @@ func _ready():
 		date_month.add_item(month_str, Month[month])
 	for day in range(1, 31):
 		date_day.add_item(str(day), day)
-	
+
 	date_year.select(1983 - YEAR_START)
 	date_month.select(Month.NOVEMBER - 1)
 	date_day.select(15 - 1)
-	
+
 	date_month.item_selected.connect(_on_month_changed)
 	time_slider.value_changed.connect(_on_time_slider_changed)
 	time_hour.text_changed.connect(_on_hour_changed)
 	time_minute.text_changed.connect(_on_minute_changed)
 	time_second.text_changed.connect(_on_second_changed)
-	
+
 	rain_slider.value_changed.connect(_on_rain_slider_changed)
 	rain.value_changed.connect(_on_rain_spinbox_changed)
 	fog_slider.value_changed.connect(_on_fog_slider_changed)
 	fog.value_changed.connect(_on_fog_spinbox_changed)
-	
+
 	save_btn.pressed.connect(_on_primary_pressed)
 	close_btn.pressed.connect(func(): show_dialog(false))
 	close_requested.connect(func(): show_dialog(false))
@@ -100,7 +100,7 @@ func _reset_values():
 	fog.value = editor.ctx.data.fog_m
 	wind_dir.value = editor.ctx.data.wind_dir
 	wind_spd.value = editor.ctx.data.wind_speed_m
-	
+
 	date_year.selected = editor.ctx.data.year - YEAR_START
 	date_month.selected = editor.ctx.data.month - 1
 	date_day.selected = editor.ctx.data.day - 1
@@ -110,7 +110,7 @@ func _reset_values():
 ## Update rain slider
 func _on_rain_spinbox_changed(val: float) -> void:
 	rain_slider.value = val
-	
+
 
 ## Update rain spinbox
 func _on_rain_slider_changed(val: float) -> void:
@@ -120,18 +120,18 @@ func _on_rain_slider_changed(val: float) -> void:
 ## Update fog slider
 func _on_fog_spinbox_changed(val: float) -> void:
 	fog_slider.value = val
-	
+
 
 ## Update fog spinbox
 func _on_fog_slider_changed(val: float) -> void:
 	fog.value = val
-	
+
 
 ## When month changes update day range.
 func _on_month_changed(idx: int) -> void:
 	date_day.clear()
 	var day_range: Array
-	if idx % 2 == 0: 
+	if idx % 2 == 0:
 		if idx == 1:
 			day_range = range(1, 29)
 		else:
@@ -167,7 +167,7 @@ func _on_second_changed(text: String) -> void:
 	else:
 		time_second.text = "%02d" % _hour
 	time_slider.value = _second + _minute * 60 + _hour * 60 * 60
-	
+
 
 ## update time values from slider
 func _on_time_slider_changed(val: float) -> void:
@@ -177,7 +177,7 @@ func _on_time_slider_changed(val: float) -> void:
 	time_hour.text = "%02d" % _hour
 	time_minute.text = "%02d" % _minute
 	time_second.text = "%02d" % _second
-	
+
 
 ## Check if string is a valid number
 func _check_if_valid_number(input: String) -> bool:
